22f5cff83106962dbf01e2871992517171451e74
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-10-02 02:03:16 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(args = {title: default_title, release_date: defaul
 
   Movie.create do |m|
     m.title = args[:title]
-    m.release_date = 1990
+    m.release_date = args[:release_date]
   end
 end
 
